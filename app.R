d6905942b6d0e1f098b1d63d1148a57eb4a2297d
Amin Adibi <adibi@alumni.ubc.ca>
2018-04-04 13:17:42 -0700

    ranges converted to sane ranges

--- app.R
+++ app.R
@@ -85,11 +85,11 @@ ui <- fluidPage(
                  shinyjs::hidden(
                    div(id = "BloodTest",
                        numericInput("hema","Hematocrit (%)",value = NULL, min = 25, max = 62, step = 1),
-                       numericInput("white_bc","White blood cells (10^9/L)", value = NULL, min = 25, max = 172, step = 0.01),
-                       numericInput("trig","Triglycerides (mg/dl)",value = NULL, min=1.77, max = 1342.25, step = 0.01),
-                       numericInput("alb","Albumin (g/L)",value = NULL, min = 24, max = 59, step = 1),
-                       numericInput("glob","Globulin (g/L)",value = NULL, min = 10, max = 49, step = 1),
-                       numericInput("alk_phos","Alkaline Phosphotase (IU/L)",value = NULL, min = 16, max = 98, step = 1),
+                       numericInput("white_bc","White blood cells (10^9/L)", value = NULL, min = 25, max = 172, step = 1),
+                       numericInput("trig","Triglycerides (mg/dl)",value = NULL, min = 1, max = 2000, step = 1),
+                       numericInput("alb","Albumin (g/L)",value = NULL, min = 10, max = 100, step = 1),
+                       numericInput("glob","Globulin (g/L)",value = NULL, min = 1, max = 100, step = 1),
+                       numericInput("alk_phos","Alkaline Phosphotase (IU/L)",value = NULL, min = 1, max = 200, step = 1),
                        downloadButton("save_inputs_button", "Save Inputs"),
                        fileInput("load_inputs_button","Choose CSV File to Load",accept = c("text/csv","text/comma-separated-values,text/plain",".csv"),buttonLabel = "Load Inputs...")
                    
